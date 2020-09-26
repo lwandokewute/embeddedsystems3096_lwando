e36e5ed59b70c7374024ac5f0dea49bacd878b4e
lwandokewute <65496247+lwandokewute@users.noreply.github.com>
2020-09-26 19:46:57 +0200

    Update toggle_led.py

--- toggle_led.py
+++ toggle_led.py
@@ -15,14 +15,12 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(16, GPIO.OUT, initial=1)
 
 #Main function for toggling the led
-"""
-def main():
-    for i in range(10):
-        time.sleep(1) # 1 second time delay
-        GPIO.output(16, GPIO.LOW)
-        time.sleep(1) # 1 second time delay
-        GPIO.output(16, GPIO.HIGH)
-"""
+#def main():
+#    for i in range(10):
+#        time.sleep(1) # 1 second time delay
+#        GPIO.output(16, GPIO.LOW)
+#        time.sleep(1) # 1 second time delay
+#        GPIO.output(16, GPIO.HIGH)
 
 #Main function for using the switch
 def main():
