1c55f0ef0a1f3bd62c9bff0449cd8d086d9dc64d
lwandokewute <65496247+lwandokewute@users.noreply.github.com>
2020-09-27 00:48:23 +0200

    Update toggle_led.py

--- toggle_led.py
+++ toggle_led.py
@@ -29,14 +29,15 @@ def main():
    
     def my_callback(channel):
         print('This is a edge event callback function!')
+        LED_NEXT_STATE != LED_NEXT_STATE
     
     GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=200)
-    
-    for i in range(8):
-         if GPIO.input(18) is 0:
-             GPIO.output(16, LED_NEXT_STATE)
-             LED_NEXT_STATE != LED_NEXT_STATE
-         time.sleep(0.5) # 0.5 second time delay
+    GPIO.output(16, LED_NEXT_STATE)
+#    for i in range(8):
+ #        if GPIO.input(18) is 0:
+  #           GPIO.output(16, LED_NEXT_STATE)
+   #          LED_NEXT_STATE != LED_NEXT_STATE
+    time.sleep(0.5) # 0.5 second time delay
     GPIO.remove_event_detect(18)
         
 
