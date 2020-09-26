eedc2752c76840fdcb64a716ff2a36a27fe6debb
lwandokewute <65496247+lwandokewute@users.noreply.github.com>
2020-09-26 18:38:15 +0200

    Update toggle_led.py

--- toggle_led.py
+++ toggle_led.py
@@ -14,38 +14,23 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #Output is pin 14, the led with initial value
 GPIO.setup(16, GPIO.OUT, initial=1)
 
-time.sleep(1) # 1 second time delay
-GPIO.output(16, GPIO.LOW)
-time.sleep(1) # 1 second time delay
-GPIO.output(16, GPIO.HIGH)
-time.sleep(1) # 1 second time delay
-GPIO.output(16, GPIO.LOW)
-time.sleep(1) # 1 second time delay
-GPIO.output(16, GPIO.HIGH)
-time.sleep(1) # 1 second time delay
-GPIO.output(16, GPIO.LOW)
-time.sleep(1) # 1 second time delay
-GPIO.output(16, GPIO.HIGH)
-time.sleep(1) # 1 second time delay
-GPIO.output(16, GPIO.LOW)
-
 #Main function
-#def main():
- #   for i in range(10):
-  #       time.sleep(1000) # 1 second time delay
-   #      GPIO.output(14, GPIO.LOW)
-    #     time.sleep(1000) # 1 second time delay
-     #    GPIO.output(14, GPIO.HIGH)
+def main():
+    for i in range(10):
+        time.sleep(1) # 1 second time delay
+        GPIO.output(16, GPIO.LOW)
+        time.sleep(1) # 1 second time delay
+        GPIO.output(16, GPIO.HIGH)
 
 #TRIAL & EXCEPTION
-#if __name__ == "__main__":
-#    # Make sure the GPIO is stopped correctly
-#    try:
-#        main()
-#    except KeyboardInterrupt:
-#        print("Exiting gracefully")
-#        GPIO.cleanup()
-#    except e:
-#        print("Some other error occurred: {}".format(e.message))
-#        GPIO.cleanup()
+if __name__ == "__main__":
+    # Make sure the GPIO is stopped correctly
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exiting gracefully")
+        GPIO.cleanup()
+    except e:
+        print("Some other error occurred: {}".format(e.message))
+        GPIO.cleanup()
 GPIO.cleanup()
