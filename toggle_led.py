7876306ba5ff58d904890955529936eb3322f7ec
lwandokewute <65496247+lwandokewute@users.noreply.github.com>
2020-09-27 01:43:33 +0200

    Update toggle_led.py

--- toggle_led.py
+++ toggle_led.py
@@ -27,34 +27,17 @@ LED_NEXT_STATE = [True]
 #Main function for using the switch
 def main():
     while True:    
-        channel = GPIO.wait_for_edge(18, GPIO.FALLING)
+        channel = GPIO.wait_for_edge(18, GPIO.FALLING , timeout=0.4)
         if channel is 0:
             print('Press detected')
             LED_NEXT_STATE[0] = not LED_NEXT_STATE[0]
             GPIO.output(16, LED_NEXT_STATE[0])
         
         GPIO.remove_event_detect(18)
-        time.sleep(0.02) # 0.02 second time delay
-        
-        #def my_callback(channel):
-            #print('This is a edge event callback function!')
-            #LED_NEXT_STATE[0] = not LED_NEXT_STATE[0]
-    
-        #GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=200)
-        #GPIO.output(16, LED_NEXT_STATE[0])
-#    for i in range(8):
- #        if GPIO.input(18) is 0:
-  #           GPIO.output(16, LED_NEXT_STATE)
-   #          LED_NEXT_STATE != LED_NEXT_STATE
-        #time.sleep(1) # 0.5 second time delay
-        #GPIO.remove_event_detect(18)
-        
-        
-        
+        time.sleep(0.5) # 0.5 second time delay
 
 #TRIAL & EXCEPTION
 if __name__ == "__main__":
-    # Make sure the GPIO is stopped correctly
     try:
         main()
     except KeyboardInterrupt:
