87fad03a2e58befd878e843169c27b12a69a2f75
lwandokewute <65496247+lwandokewute@users.noreply.github.com>
2020-09-27 02:54:59 +0200

    Update toggle_led.py

--- toggle_led.py
+++ toggle_led.py
@@ -26,10 +26,9 @@ LED_NEXT_STATE = [True]
 
 #Main function for using the switch
 def main():
-    print("Running main loop:")
-    while True: 
-        
-        time.sleep(0.5) # 0.5 second time delay
+    
+    while True:     
+        time.sleep(1) # 1 second time delay
         
         #WAITING FOR THE BUTTON TO BE PRESSED
         channel = GPIO.wait_for_edge(18, GPIO.FALLING)
