c78fef6bf66d0e81b2f393c1faa5a02e4ea20eb1
lwandokewute <65496247+lwandokewute@users.noreply.github.com>
2020-09-27 02:10:05 +0200

    Update toggle_led.py

--- toggle_led.py
+++ toggle_led.py
@@ -27,7 +27,7 @@ LED_NEXT_STATE = [True]
 #Main function for using the switch
 def main():
     while True:    
-        channel = GPIO.wait_for_edge(18, GPIO_FALLING, timeout=1000)
+        channel = GPIO.wait_for_edge(18, GPIO.FALLING, timeout=5000)
         if channel is 0:
             print('Press detected')
             LED_NEXT_STATE[0] = not LED_NEXT_STATE[0]
